c5edf4e442fa1ec19102aa8e54c506774cbef8c8
Dániel Móna <mona.daniel97@gmail.com>
2019-07-10 23:09:48 +0200

    Add antenna rotation calculation

--- radio_station.py
+++ radio_station.py
@@ -1,10 +1,13 @@
 import sys
 import json
 import re
+import math
 
 import asyncio
 import serial_asyncio
 import websockets
+
+E_RADIUS = 6371000
 
 UPRA_TLMPACKET_FMT = (
     r'\$\$(?P<csgn>.{7}),'
@@ -22,9 +25,13 @@ UPRA_TLMPACKET_FMT = (
     r'(?P<comtemp>.{3}),'
 )
 
-station_lat = None
-station_lng = None
-station_alt = None
+station_lat = None # Station latitude, decimal degrees
+station_lng = None # Station longitude, decimal degrees
+station_alt = None # Station altitude, metres
+
+antenna_azimuth = None # Antenna azimuth, decimal degrees
+antenna_elevation = None # Antenna elevation, decimal degrees
+target_distance = None # Distance of tracked target from ARS, metres
 
 def input_type(msg, type_converter):
     while True:
@@ -45,6 +52,37 @@ async def setup_location(websocket):
         'lng': station_lng
         'alt': station_alt
     }))
+
+async def calc_rotation_for_hab(hab_lat, hab_lng, hab_alt):
+    """
+    Compute antenna rotation using received location of a tracked high altitude balloon
+    """
+    hab_lat_rad = math.radians(hab_lat)
+    hab_lng_rad = math.radians(hab_lng)
+    ars_lat_rad = math.radians(station_lat)
+    ars_lng_rad = math.radians(station_lng)
+
+    d_lng = hab_lng_rad - ars_lng_rad
+
+    sa = (math.cos(hab_lat_rad) * math.sin(d_lng))
+    sb = ((math.cos(ars_lat_rad) * math.sin(hab_lat_rad)) - (math.sin(ars_lat_rad) * math.cos(hab_lat_rad) * math.cos(d_lng)))
+
+    antenna_azimuth = math.degrees(math.atan2(sa, sb))
+
+    aa = math.hypot(sa, sb)
+    ab = (math.sin(ars_lat_rad) * math.sin(hab_lat_rad)) + (math.cos(ars_lat_rad) * math.cos(hab_lat_rad) * math.cos(d_lng))
+    angle_at_centre = math.atan2(aa, ab)
+    great_circle_distance = angle_at_centre * E_RADIUS
+
+    ta = E_RADIUS + station_alt
+    tb = E_RADIUS + hab_alt
+    ea = (math.cos(angle_at_centre) * tb) - ta
+    eb = math.sin(angle_at_centre) * tb
+    antenna_elevation_rad = math.atan2(ea, eb)
+    antenna_elevation = (antenna_elevation_rad < 0) ? 0 : math.degrees(antenna_elevation_rad)
+
+    target_distance = math.sqrt((ta * ta) + (tb * tb) - 2 * tb * ta * math.cos(angle_at_centre))
+
 
 async def pick_mission(websocket):
     await websocket.send(json.dumps({
